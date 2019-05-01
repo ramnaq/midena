14d3cee6aa1d8015e0c080ecddadd0e7c1217668
Ramna Sidharta <ramna.thors@gmail.com>
2019-05-01 16:04:27 -0300

    Commet testing import_FA in app.py

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ presenter = MainPresenter(view)
 
 # Just testing
 # fa = import_FA('fa_written.json')
-fa = import_FA('tests/primeiroultimo.json')
+#fa = import_FA('tests/primeiroultimo.json')
 #fa.sigma = ['a', 'b', 'c']
 # fa.export_to('fa_written.json')
-
-print(fa)
+#print(fa)
 # end
 
 view.show()
