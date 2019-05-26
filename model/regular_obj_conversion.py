2e04e47bfc8fcec3a9d9bcc22012d56d094f4d62
Ramna Sidharta <ramna.thors@gmail.com>
2019-05-26 16:13:22 -0300

    RG->FA conversion correction, 'accepting' still wrong

---
+++ model/regular_obj_conversion.py
@@ -0,0 +1,63 @@
+from model.RegularGrammar import RegularGrammar
+from model.FiniteAutomata import FiniteAutomata
+
+def regular_grammar_to_automata(grammar):
+    name = grammar.name + "FiniteAutomata"
+    initial = grammar.root
+    table = {}
+    sigma = set()
+    accepting  = set()
+    for prod in grammar.productions:
+        state = prod[0]
+        table[state] = {}
+        for beta in prod[1]:
+            symbol = beta[0]
+            sigma.add(symbol)
+            if len(beta) == 1:
+                accepting.add(state)
+            else:
+                table[state][symbol] = beta[1]
+    return FiniteAutomata(list(sigma), table, initial, list(accepting))
+
+def finite_automata_to_grammar(automata):
+    name = automata.name + "Grammar"
+    root = automata.initial
+    sigma = set(automata.sigma)
+
+    faStates = automata.states()
+    grammarStates = list(map(lambda s: grammar_symbol_form(s), faStates))
+    symbols = set(grammarStates)
+
+    productions = []
+
+    for state in faStates:
+        stateTransictions = automata.table[state]
+        alpha = grammar_symbol_form(state)
+        beta = []
+        for symbol in automata.sigma:
+            if symbol in stateTransictions.keys():
+                nextStates =\
+                        next_states_separated(stateTransictions, symbol)
+                beta += list(map(
+                            lambda ns: symbol + grammar_symbol_form(ns),\
+                            nextStates)
+                        )
+                add_terminal_symbol(
+                        symbol, beta, nextStates, automata.accepting)
+        productions.append((alpha, beta))
+
+    return RegularGrammar(symbols, sigma, productions, root, name)
+
+def add_terminal_symbol(symbol, beta, nextStates, accepting):
+    if list(filter(lambda s: s in accepting, nextStates)):
+        beta.append(symbol)
+
+def grammar_symbol_form(state):
+    return "_".join(state.upper())
+
+def next_states_separated(transitions, symbol):
+    nextStates = []
+    for ns in transitions[symbol]:
+        nextStates += ns.split(", ")
+    return nextStates
+
